55ea7f494db1b4798c0aec554a6655713320c7d3
Mu <mu7147@saskpolytech.ca>
2024-01-03 13:53:43 -0600

    recursive

--- src/COSC602/Class0103/TestRecursion.java
+++ src/COSC602/Class0103/TestRecursion.java
@@ -5,9 +5,15 @@ public class TestRecursion
     public static void main(String[] args)
     {
         System.out.println(factorial(4));
+        System.out.println(fib(4));
     }
     public static int factorial(int n){
         if(n == 0) return 1;
         return n * factorial(n - 1);
+    }
+    public static long fib(long n){
+        if(n == 1) return 1;
+        if(n == 0) return 1;
+        return fib(n - 1) + fib(n - 2);
     }
 }
