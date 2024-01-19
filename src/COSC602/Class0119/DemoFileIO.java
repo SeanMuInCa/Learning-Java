6eda04a12ee25bc8c41b2a0b3a09093943cea20a
Mu <mu7147@saskpolytech.ca>
2024-01-19 14:47:13 -0600

    io

---
+++ src/COSC602/Class0119/DemoFileIO.java
@@ -0,0 +1,36 @@
+package COSC602.Class0119;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class DemoFileIO
+{
+    public static void main(String[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        try(
+                Scanner input = new Scanner(new File("d:\\test.txt"));
+                )
+        {
+            while (input.hasNext())
+            {
+                String str = input.nextLine();
+                sb.append("\r\n").append(str.replaceAll("a", "b"));
+            }
+            System.out.println(sb);
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        try(PrintWriter printWriter = new PrintWriter(new File("d:\\test.txt")))
+        {
+            printWriter.print(sb);
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
