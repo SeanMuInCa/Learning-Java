c08978c6f6c4102439a9b366d8604b33e74cb297
Mu <105630173+SeanMuInCa@users.noreply.github.com>
2024-03-18 13:52:48 -0600

    Add files via upload

---
+++ src/COSC603/Class0318/TaskThreadDemo.java
@@ -0,0 +1,51 @@
+package COSC603.Class0318;
+
+public class TaskThreadDemo
+{
+    public static void main(String[] args)
+    {
+        PrintChar p = new PrintChar('a',100);
+        PrintChar p1 = new PrintChar('b',100);
+        PrintNum p2 = new PrintNum(100);
+        new Thread(p).start();
+        new Thread(p1).start();
+        new Thread(p2).start();
+    }
+}
+class PrintChar implements Runnable
+{
+    private final char charToPrint;
+    private final int times;
+    public PrintChar(char c, int t)
+    {
+        this.charToPrint = c;
+        this.times = t;
+    }
+    @Override
+    public void run()
+    {
+        for (int i = 0; i < times; i++)
+        {
+            System.out.print(charToPrint);
+        }
+    }
+}
+
+class PrintNum implements Runnable
+{
+    private final int lastNum;
+
+    public PrintNum(int lastNum)
+    {
+        this.lastNum = lastNum;
+    }
+
+    @Override
+    public void run()
+    {
+        for (int i = 0; i < lastNum; i++)
+        {
+            System.out.print(" " + i);
+        }
+    }
+}
